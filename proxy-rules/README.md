39728ee5aeb196b653bb3721fc942dab4e468fab
T3ss <9061401+t3spe@users.noreply.github.com>
2022-01-16 21:50:07 -0800

    commit build script

---
+++ proxy-rules/README.md
@@ -0,0 +1,4 @@
+rules from here: 
+https://github.com/naveednajam/Little-Snitch---Rule-Groups/tree/master/unified_hosts_fakenews-gambling-social
+
+taking all the goodstuff and just removing linkedin and reddit (which still qualify as social, but are somewhat usefull)
---
+++ proxy-rules/build_list.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+export SELFDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+ORIGIN="https://raw.githubusercontent.com/naveednajam/Little-Snitch---Rule-Groups/master/unified_hosts_fakenews-gambling-social/sb_unified_hosts_fakenews-gambling-social.lsrules"
+RAW_FILE="raw.lsrules"
+echo "retrieving origin file ${ORIGIN}"
+wget -O ${SELFDIR}/${RAW_FILE} ${ORIGIN}
+echo "masking linkedin and reddit..."
+# this could be done better by removing the rules - but keep it simple for now
+cat ${SELFDIR}/${RAW_FILE} | sed 's/linkedin/masked001linkedin/' | sed 's/reddit/masked002reddit/' >  ${SELFDIR}/sb_unified_hosts_fakenews-gambling-social.lsrules
+echo "done"
