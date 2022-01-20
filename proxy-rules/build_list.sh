48670fb70053d415ec383a456d61263e4355dde2
T3ss <9061401+t3spe@users.noreply.github.com>
2022-01-19 20:52:43 -0800

    update reddit/linkedin rules with some exceptions

--- proxy-rules/build_list.sh
+++ proxy-rules/build_list.sh
@@ -6,5 +6,5 @@ echo "retrieving origin file ${ORIGIN}"
 wget -O ${SELFDIR}/${RAW_FILE} ${ORIGIN}
 echo "masking linkedin and reddit..."
 # this could be done better by removing the rules - but keep it simple for now
-cat ${SELFDIR}/${RAW_FILE} | sed 's/linkedin/masked001linkedin/' | sed 's/reddit/masked002reddit/' >  ${SELFDIR}/sb_unified_hosts_fakenews-gambling-social.lsrules
+cat ${SELFDIR}/${RAW_FILE} | sed 's/linkedin/masked001linkedin/' | sed 's/\.licdn/\.masked003/' | sed 's/reddit/masked002reddit/' | sed 's/redd\.it/masked004red\.it/' >  ${SELFDIR}/sb_unified_hosts_fakenews-gambling-social.lsrules
 echo "done"
